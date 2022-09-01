aa7b40923200a6a6b662de92b87ef2767060133f
henriquenascimento01 <henriquenascimento300@hotmail.com>
2022-09-01 20:14:56 -0300

    Iniciando o front-end

--- register.php
+++ register.php
@@ -4,7 +4,7 @@ require __DIR__.'/vendor/autoload.php';
 
 // Validação do post
 if(isset($_POST['titulo'], $_POST['descricao'], $_POST['ativo'])){
-    die('cadastrar');
+    
 }
 
 include __DIR__.'/includes/header.php';
